2421cc85a005e1154224cda73b18451fb1c319c5
Pain <agibalov.sd@phystech.edu>
2021-04-03 00:20:26 +0300

    The first step of MedicalService: 1) Added required privat variables 2) Development of basic methods has begun

--- MedicalService.cpp
+++ MedicalService.cpp
@@ -3,23 +3,54 @@
 //
 
 #include "MedicalService.h"
+#include "TheArk.h"
+#include "Population.h"
+#include "BiologicalService.h"
+#include "SocialService.h"
+#include <cmath>
 
-MedicalService::MedicalService()
-{
+MedicalService::MedicalService() {
+    for (int i = 0; i < TheArk::get_instance()->getPopulation()->getOldmen(); i++)
+        Old.push_back(10 + rand() % 70);
+    for (int i = 0; i < TheArk::get_instance()->getPopulation()->getAdults(); i++)
+        Adult.push_back(20 + rand() % 80);
+    for (int i = 0; i < TheArk::get_instance()->getPopulation()->getChildren(); i++)
+        Children.push_back(40 + rand() % 60);
+}
+
+void MedicalService::process_accident(AccidentSeverity as) {
 
 }
 
-void MedicalService::process_accident(AccidentSeverity as)
-{
+void MedicalService::process_year() {
+    Old.sort();
+    Adult.sort();
+    Children.sort();
+    unsigned int NumbOld = TheArk::get_instance()->getPopulation()->getOldmen();
+    unsigned int NumbAd = TheArk::get_instance()->getPopulation()->getAdults();
+    unsigned int NumbCh = TheArk::get_instance()->getPopulation()->getChildren();
 
+    for (; NumbOld != Old.size();) {
+        if (NumbOld > Old.size()) Old.push_back(10 + rand() % 70);
+        if (NumbOld < Old.size()) Old.pop_back();
+    }
+    for (; NumbAd != Adult.size();) {
+        if (NumbAd > Adult.size()) Adult.push_back(30 + rand() % 70);
+        if (NumbAd < Adult.size()) Adult.pop_back();
+    }
+    for (; NumbCh != Children.size();) {
+        if (NumbCh > Children.size()) Children.push_back(40 + rand() % 60);
+        if (NumbCh < Children.size()) Children.pop_back();
+    }
+    State =
 }
 
 unsigned int MedicalService::borderAdultsToOldmen() {
-    return 65;
+    return retirementAge;
 }
 
 double MedicalService::deathRateChildren() {
-    return 0.00001;
+    return 0.0001;
 }
 
 double MedicalService::deathRateAdult() {
--- MedicalService.h
+++ MedicalService.h
@@ -6,14 +6,24 @@
 #define THE_ARK_MEDICALSERVICE_H
 
 #include "Service.h"
+#include <list>
+using std::list;
 
 class MedicalService : public Service {
 private:
+    list<unsigned int> Children;
+    list<unsigned int> Adult;
+    list<unsigned int> Old;
+    double State;
+    unsigned int retirementAge;
+    double ChildrenDeath;
+    double AdultDeath;
+    double OldDeath;
 public:
     MedicalService();
 
     void process_accident(AccidentSeverity as) override;    // каждая служба должна уметь в своих терминах обработать переданную ей аварию
-    void process_year() override {};                        // если у службы есть какая-то личная жизнь, она может заниматься ей тут
+    void process_year() override;                        // если у службы есть какая-то личная жизнь, она может заниматься ей тут
     double getState() override;                             // каждая служба должна уметь вернуть свое состояние в процентах, посчитав его в своих терминах
     void setState(double s) override;                       // функция для инициализации, каждая служба должна уметь получить состояние в процентах и пересчитать  его в своих терминах
 
@@ -21,6 +31,7 @@ public:
     double deathRateChildren();             // средняя вероятность, что один ребенок подгибнет в течение года
     double deathRateAdult();                // средняя вероятность, что один взрослый подгибнет в течение года
     double deathRateOldmen();               // средняя вероятность, что один старик подгибнет в течение года
+
 };
 
 
