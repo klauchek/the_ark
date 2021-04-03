db08d1e1551251a5a2e66f4fab85629ca9f158eb
Pain <agibalov.sd@phystech.edu>
2021-04-03 23:59:01 +0300

    The first+ step of MedicalService: 1) Fixed Segfault P.S. Don't use "setState", it's not work!

--- MedicalService.cpp
+++ MedicalService.cpp
@@ -7,16 +7,9 @@
 #include "Population.h"
 #include "BiologicalService.h"
 #include "SocialService.h"
-#include <cmath>
 
-MedicalService::MedicalService() {
-    for (int i = 0; i < TheArk::get_instance()->getPopulation()->getOldmen(); i++)
-        Old.push_back(10 + rand() % 70);
-    for (int i = 0; i < TheArk::get_instance()->getPopulation()->getAdults(); i++)
-        Adult.push_back(20 + rand() % 80);
-    for (int i = 0; i < TheArk::get_instance()->getPopulation()->getChildren(); i++)
-        Children.push_back(40 + rand() % 60);
-}
+MedicalService::MedicalService() : retirementAge(65), ChildrenDeath(0.0001), AdultDeath(0.001), OldDeath(0.01),
+                                   State(100) {}
 
 void MedicalService::process_accident(AccidentSeverity as) {
 
@@ -42,7 +35,6 @@ void MedicalService::process_year() {
         if (NumbCh > Children.size()) Children.push_back(40 + rand() % 60);
         if (NumbCh < Children.size()) Children.pop_back();
     }
-    State =
 }
 
 unsigned int MedicalService::borderAdultsToOldmen() {
@@ -66,5 +58,11 @@ double MedicalService::getState() {
 }
 
 void MedicalService::setState(double s) {
+    for (int i = 0; i < TheArk::get_instance()->getPopulation()->getOldmen(); i++)
+        Old.push_back(10 + rand() % 70);
+    for (int i = 0; i < TheArk::get_instance()->getPopulation()->getAdults(); i++)
+        Adult.push_back(20 + rand() % 80);
+    for (int i = 0; i < TheArk::get_instance()->getPopulation()->getChildren(); i++)
+        Children.push_back(40 + rand() % 60);
 
 }
