fb0fb10997a45a447b2b0e1d6dbb22fe048f7f84
KnyazM <74839415+klauchek@users.noreply.github.com>
2021-04-16 17:01:27 +0300

    emergency in process (not working)

--- EmergencyService.cpp
+++ EmergencyService.cpp
@@ -1,23 +1,119 @@
 //
 // Created by Amisto on 4/2/2021.
+
+
+//Каждая служба должна уметь в своих терминах обрабатывать аварийные ситуации по запросу от ЧС.
+//Например, ЧС говорит, что авария средней тяжести произошла в БС.БС должна уметь в своих терминах получить соответствующий урон
+
+
+//Могут ли на самой ЧС случаться аварии? Что влияет на состояние ЧС? А что влияет на состояние других служб, кроме аварий?
+//Как реализовать то, что разные службы могут вызывать аварии ДРУГ У ДРУГА?
+
+// какие нужны ресурсы, если нужны?  оружие! инструменты!
+
+//что значит прекращать аварию? как прекращать? - не прекращать, они сами 
+
+//службы чинят сами себя - мы им только передаем аварии
 //
+//да, на чс могут случаться аварии 
+//
+//человек 100, ресурсы, случаются ли аварии - это определяет состояние - задается снаружи, пользовательский ввод или интерфейс (сколько их) --- // на что они тратятся?
+//
+//
+//восполнять - делать запрос - дадут
+
+//
+//что такое 60 - сама решает, что это такое
 
 #include "EmergencyService.h"
 
-EmergencyService::EmergencyService()
+EmergencyService::EmergencyService() : State(100)
+{}
+
+
+//интерфейс и вероятность аварий? Чем хуже состояние службы - тем выше вероятность аварии (рассчитывать эту зависимость)
+//Чем хуже состояние ЧС - тем выше тяжесть аварии
+
+//от 0 до 1
+double EmergencyService::accident_propability()
+{
+    double s = TheArk::get_instance()->getMedicalService()->getState() / 100;
+    return 1 / pow(3, 3 * s);
+    //обратнопропорцинонально состоянию самой службы
+}
+
+//от 1 до 100
+unsigned int EmergencyService::damage_factor()
+{
+    if (this->getState() != 0)
+        return 100 / (this->getState());
+    else return 100;
+    //чем лучше состояние чс, тем меньше коэф -> меньше ущерб
+}
+
+
+//для создания своих
+
+void EmergencyService::create_accident()
+{
+    srand(time(0));
+    int temp = (20 + rand() % 100);
+    double k = temp * (1 - this->accident_propability());
+    if (k > 20 && k < 100)
+        this->determine_severity();
+    else return;
+}
+
+//хочется иметь массив указателей/ссылок на службы и передавать в функции; в цикле вызывать для каждой из 6
+void EmergencyService::determine_severity()
+{
+    if (this->damage_factor() > 0 && this->damage_factor() < 11)
+        TheArk::get_instance()->getMedicalService()->process_accident(NEGLIGIBLE);
+
+    else if (this->damage_factor() > 10 && this->damage_factor() < 31)
+        TheArk::get_instance()->getMedicalService()->process_accident(LIGHT);
+
+    else if (this->damage_factor() > 30 && this->damage_factor() < 51)
+        TheArk::get_instance()->getMedicalService()->process_accident(MEDIUM);
+
+    else if (this->damage_factor() > 50 && this->damage_factor() < 71)
+        TheArk::get_instance()->getMedicalService()->process_accident(SEVERE);
+
+    else if (this->damage_factor() > 70 && this->damage_factor() < 91)
+        TheArk::get_instance()->getMedicalService()->process_accident(DISASTROUS);
+
+    else if (this->damage_factor() > 90 && this->damage_factor() < 101)
+        TheArk::get_instance()->getMedicalService()->process_accident(CATASTROPHIC);
+}
+
+
+void EmergencyService:: process_year()
 {
 
 }
 
+//для обработки переданных
 void EmergencyService::process_accident(AccidentSeverity as)
 {
+    if (NEGLIGIBLE)
+    {
+
+    }
 
 }
 
-double EmergencyService::getState() {
-    return 100;
+double EmergencyService::getState()
+{
+    return State;
 }
 
-void EmergencyService::setState(double s) {
-
+void EmergencyService::setState(double s)
+{
+    State = s;
 }
+
+//unsigned int EmergencyService::getResourceDemand()
+//{}
+//unsigned int EmergencyService::getStaffDemand()
+//{}
+
--- EmergencyService.h
+++ EmergencyService.h
@@ -5,18 +5,35 @@
 #ifndef THE_ARK_EMERGENCYSERVICE_H
 #define THE_ARK_EMERGENCYSERVICE_H
 
+#include <ctime>
 #include "Service.h"
+#include "TheArk.h"
+#include "MedicalService.h"
+#include "NavigationService.h"
+#include "BiologicalService.h"
+#include "SocialService.h"
+#include "TechnicalService.h"
+
+
 
 class EmergencyService : public Service {
 private:
+    double State;
+
 public:
     EmergencyService();
 
+    double accident_propability();
+    unsigned int damage_factor();
+    void determine_severity();
+    void create_accident();
     void process_accident(AccidentSeverity as) override;    // каждая служба должна уметь в своих терминах обработать переданную ей аварию
-    void process_year() override {};                        // если у службы есть какая-то личная жизнь, она может заниматься ей тут
+    void process_year() override;                   // если у службы есть какая-то личная жизнь, она может заниматься ей тут
     double getState() override;                             // каждая служба должна уметь вернуть свое состояние в процентах, посчитав его в своих терминах
     void setState(double s) override;                       // функция для инициализации, каждая служба должна уметь получить состояние в процентах и пересчитать  его в своих терминах
 
+    //unsigned int getResourceDemand();           // сколько ресурсов требуется
+    //unsigned int getStaffDemand();              // сколько людей требуется
 };
 
 
